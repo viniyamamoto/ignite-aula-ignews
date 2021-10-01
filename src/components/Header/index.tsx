27d47491085e7eae0407064f955f868d773cba56
Vinicios Koji Yamamoto <viniciosyamamoto@gmail.com>
2021-10-01 14:12:44 -0300

    Completed project

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,24 @@
-import { SingInButton } from '../SingInButton';
-import styles from './styles.module.scss';
+import { SingInButton } from '../SingInButton'
+import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
-    return(
+    return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active} href="#">Home</a>
-                    <a href="#">Posts</a>
+
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink href="/posts" activeClassName={styles.active}>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SingInButton />
             </div>
         </header>
-    );
+    )
 }
